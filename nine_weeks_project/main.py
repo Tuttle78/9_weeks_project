175dcb4f7b4cc6d16e03ee4861f8301aec11f6db
Tuttle78 <zt539170@georgetownisd.org>
2026-01-30 13:31:18 -0600

    Starting money and ingredients

---
+++ nine_weeks_project/main.py
@@ -0,0 +1,4 @@
+import shop
+
+money=int(input("How much money would you like to start with? "))
+print(shop.ingredients(0, 0, 0))
---
+++ nine_weeks_project/shop.py
@@ -0,0 +1,4 @@
+def ingredients(lemons, sugar, ice_cubes):
+    return {"Lemons": lemons,
+            "Sugar": sugar,
+            "Ice cubes": ice_cubes}
