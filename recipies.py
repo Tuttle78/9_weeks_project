42a9ecd74884cbcd48bbc245960f7e085beb3e83
K3nder <rs558990@georgetownisd.org>
2026-02-10 13:38:19 -0600

    Updated recipes, needs revising

--- recipies.py
+++ recipies.py
@@ -1,7 +1,10 @@
 import shop
 
-def recipe()
+def recipe():
+    times_put_in = ()
+    lemons = 0.25
     recipe=[]
     recipe.append(int(input("How many lemons? ")))
-    if recipe > 0:
+    if recipe > 2:
+        input("you can only have atmost of 2 of ingredients each one ingredient is ALWAYS worth 0.25 out of 2")
         
