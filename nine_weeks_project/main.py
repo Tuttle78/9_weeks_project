8e4c385c839b90d1ca49e34bb93f5ec8911dd1be
K3nder <rs558990@georgetownisd.org>
2026-02-10 12:52:23 -0600

    updates
    updates

--- nine_weeks_project/main.py
+++ nine_weeks_project/main.py
@@ -1,4 +1,16 @@
 import shop
+import customer
 
-money=int(input("How much money would you like to start with? "))
-print(shop.ingredients(0, 0, 0))
+try:
+    money=int(input("How much money would you like to start with? \n"))
+    print(shop.ingredients(0, 0, 0))
+except TypeError:
+    input("Sorry i didnt get that")
+else:
+    start = input("Would you like to start the day? \n")
+
+if start == "yes":
+    input("Great!")
+    print("--DAY 1-- \n")
+    print(shop.ingredients(0, 0, 0))    
+    input(customer.customer)
