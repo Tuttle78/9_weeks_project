871d7763a1b1be5cae5f2c3091ca3a806a8b5904
MakeCoder9346 <ey534018@georgetownisd.org>
2026-02-11 12:50:34 -0600

    Added extra choices for the game

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import shop
-money=0
+import recipe
+
+money = 0
+
 print("Welcome to the lemonade stand game")
 diff_choice = (input("What difficulty are you chosing 1. easy 2. medium 3. hard:\n"))
 if diff_choice == 1 or "easy" or "Easy":
-    shop.ingredients(500, 500, 450)
-    money=2000
+    inventory_dictionary["lemons"] = 500 ["sugar"] = 500 ["ice_cubes"] = 400
+    money = 2000
 if diff_choice == 2 or "medium" or "Medium":
-    shop.ingredients(250, 250, 200)
-    money=500
+    inventory_dictionary["lemons"] = 250 ["sugar"] = 250 ["ice_cubes"] = 200
+    money = 500
 if diff_choice == 3 or "hard" or "Hard":
-    shop.ingredients(100, 100, 50)
-    money=200
+    inventory_dictionary["lemons"] = 100 ["sugar"] = 100 ["ice_cubes"] = 50
+    money = 200
+
 game_choices = int(input("What would you like to do 1. recipe 2. start the day 3. see inventory 4. shop:\n"))
 if game_choices == 3:
     print("You have",shop.ingredients)
+if game_choices == 1:
+    game_recipe_1 = int(input("How much lemons do you want for each cup:\n"))
+    game_recipe_2 = int(input("How much sugar do you want for each cup:\n"))
+    game_recipe_3 = int(input("How much ice cubes do you want for each cup:\n"))
+    recipe.recipe(game_recipe_1, game_recipe_2, game_recipe_3)
