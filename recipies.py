8f74929fd8de67916268a56c720a624a3ccdbe42
plasma1011 <jm558913@georgetownisd.org>
2026-02-11 13:09:36 -0600

    Update recipies.py

--- recipies.py
+++ recipies.py
@@ -2,12 +2,18 @@ import shop
 
 def recipe():
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     times_put_in = ()
     lemons = 0.25
     recipe=[]
     recipe.append(int(input("How many lemons? ")))
     if recipe > 2:
         input("you can only have atmost of 2 of ingredients each one ingredient is ALWAYS worth 0.25 out of 2")
+=======
+    recipe=[]
+    recipe.append(int(input("How many lemons? ")))
+    if recipe > 1:
+>>>>>>> Stashed changes
 =======
     recipe=[]
     recipe.append(int(input("How many lemons? ")))
