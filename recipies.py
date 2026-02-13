7193328cc816ec137d07920f23644adf6fb9857a
K3nder <rs558990@georgetownisd.org>
2026-02-13 13:29:49 -0600

    updated recipes.py
    added limits to inventory, for lemons, sugar, and icecubes

--- recipies.py
+++ recipies.py
@@ -1,9 +1,15 @@
+import main
+
 def recipe():
     recipe=[]
     recipe.append(int(input("How many lemons do you want for each cup:\n")))
     recipe.append(int(input("How much sugar do you want for each cup:\n")))
     recipe.append(int(input("How many ice cubes do you want for each cup:\n")))
-    print("Your recipe is", recipe)
+    if recipe(0) == 1000 or recipe(1) == 1000 or recipe(2) == 1000:
+        print("You have too much lemons, sugar, or icecubes")
+    print("Your recipe is\n", recipe)
+
+        
     
 
         
