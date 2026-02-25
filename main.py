f498c1ad1890124447f1c0c267a1fdd643a115b1
Tuttle78 <zt539170@georgetownisd.org>
2026-02-25 13:07:15 -0600

    added game loop

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import recipies
-# import day
 
+day_num=0
 money = 0
+days=0
 inventory_dictionary={"lemons": 0,
                       "sugar": 0,
                       "ice_cubes":0
@@ -20,48 +21,46 @@ def shop(inventory_dictionary):
         inventory_dictionary['ice_cubes']+ice_cubes
 
 print("Welcome to the lemonade stand game")
-diff_choice = (input("What difficulty are you chosing 1. easy 2. medium 3. hard:\n"))
-if diff_choice == 1 or "easy" or diff_choice == "Easy":
+diff_choice = (input("What difficulty are you chosing? (1 = easy, 2 = medium, 3 = hard)\n"))
+if diff_choice == "1" or diff_choice == "Easy":
     inventory_dictionary['lemons'] = 500 
     inventory_dictionary['sugar'] = 500
     inventory_dictionary['ice_cubes'] = 400
     money = 2000
-elif diff_choice == 2 or "medium" or diff_choice == "Medium":
+    days=42
+elif diff_choice == "2" or diff_choice == "Medium":
     inventory_dictionary['lemons'] = 250  
     inventory_dictionary['sugar'] = 250
     inventory_dictionary['ice_cubes'] = 200
     money = 500
-elif diff_choice == 3 or "hard" or diff_choice == "Hard":
-    inventory_dictionary['lemons'] = 100  
+    days=28
+elif diff_choice == "3" or diff_choice == "Hard":
+    inventory_dictionary['lemons'] = 100
     inventory_dictionary['sugar'] = 100
     inventory_dictionary['ice_cubes'] = 50
-
     money = 200
-
-game_choices = int(input("What would you like to do? (1=recipe, 2=start the day, see inventory, 4=shop:\n"))
-if game_choices == 3:
-
-    game_choices = int(input("What would you like to do 1. recipe 2. start the day 3. see inventory 4. shop:\n"))
-if game_choices == 3:
-    print("You have", inventory_dictionary)
-elif game_choices == 1:
-    recipies.recipe()
-elif game_choices == 4:
-    shop.shop()
-#if game_choices == 2:
-#    day.day_start()
-    print("You have", inventory_dictionary)
-if game_choices == 1:
+    days=14
+else:
+    print("invalid, quitting game")
+while day_num<days:
+    game_choices = int(input("What would you like to do? (1 = recipe, 2 = start the day, 3 = see inventory, = 4 shop)\n"))
+    if game_choices == 3:
+        print("You have", inventory_dictionary)
+        continue
+    elif game_choices == 1:
         recipies.recipe()
-if game_choices == 2:
-        start = input("Starting the day, would you like to open the shop?\n")
+    elif game_choices == 4:
+        shop.shop()
 
-if start == "yes":
-      input("Opening your shop")
-      input("Shop opened, customers rolling in!")
-
-game_choices = int(input("What would you like to do 1. recipe 2. start the day 3. see inventory 4. shop:\n"))
-if game_choices == 3:
-    print("You have", inventory_dictionary)
-if game_choices == 1:
-    recipies.recipe()
+        print("You have", inventory_dictionary)
+    if game_choices == 1:
+            recipies.recipe()
+    if game_choices == 2:
+        day_num+=1
+        print("Day", day_num)
+        print("Opening your shop")
+        print("Shop opened, customers rolling in!")
+    if game_choices == 3:
+        print("You have", inventory_dictionary)
+    if game_choices == 1:
+        recipies.recipe()
