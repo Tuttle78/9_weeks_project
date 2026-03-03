e2d268fe094d7d7c03d90c29f6e0ca5b1a2afb0d
Tuttle78 <zt539170@georgetownisd.org>
2026-03-03 10:23:33 -0600

    added one story

---
+++ story.py
@@ -0,0 +1,4 @@
+def easy():
+    input("The new Call of Dut- I mean Signal of Job™ came out and all your friends got it. (space to continue)")
+    input("Unfortunately for you, your parents told you they wouldn't buy it.")
+    input("So you decide to start saving up, and what better way to raise money, but a lemonade stand!")
