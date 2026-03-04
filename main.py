147e3111b3b68914c90610bb1769ee195dfaf30c
Tuttle78 <zt539170@georgetownisd.org>
2026-03-04 10:21:10 -0600

    Bug fixes and shop additions **UNFINISHED**

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from shop import shop_screen
 
 goal=0
 day_num=0
-
+money=0
 days=0
 inventory_dictionary={"lemons": 0,
                       "sugar": 0,
                       "ice_cubes" : 0,
-                      "money" : 0
                       }
 
 print("Welcome to the lemonade stand game")
@@ -20,26 +19,32 @@ if diff_choice == "1" or diff_choice == "Easy":
     money = 60
     goal=300
     days=42
+
 elif diff_choice == "2" or diff_choice == "Medium":
     inventory_dictionary['lemons'] = 250  
     inventory_dictionary['sugar'] = 250
     inventory_dictionary['ice_cubes'] = 200
     money = 500
     days=28
+
 elif diff_choice == "3" or diff_choice == "Hard":
     inventory_dictionary['lemons'] = 100
     inventory_dictionary['sugar'] = 100
     inventory_dictionary['ice_cubes'] = 50
     money = 200
     days=14
+
 else:
     print("invalid, quitting game")
+
 while day_num<days:
     game_choices = int(input("What would you like to do? (1 = recipe, 2 = start the day, 3 = see inventory, = 4 shop)\n"))
     if game_choices == 3:
         print("You have", inventory_dictionary)
+
     elif game_choices == 4:
         shop_screen(inventory_dictionary)
+    
     if game_choices == 2:
         day_num+=1
         print("Day", day_num)
--- shop.py
+++ shop.py
@@ -3,20 +3,22 @@ def shop_screen(inventory):
     choice=input("What would you like to buy? (1: lemon, 2: sugar, 3: ice cubes)\n")
 
     if choice == "1":
-        amount = int(input("how many lemons do you want?\n"))
-        inventory['lemons'] += amount
-        if inventory["money"] < amount*5:
+        amount = int(input("how many lemons do you want? ($1 each)\n"))
+        
+        if inventory["money"] < amount:
             print("You can't afford that.")
         else:
-            inventory["money"] -= amount*5
-    
+            inventory["money"] -= amount
+            inventory['lemons'] += amount
 
 
     if choice == "2":
-        amount = int(input("how much sugar do you want?\n"))
-        inventory['sugar'] += amount
-        inventory["money"] -= amount*5
-
+        amount = int(input("how many sugar packs do you want?\n"))
+        if inventory["money"] < amount:
+            print("You can't afford that.")
+        else:
+            inventory["money"] -= amount
+            inventory['sugar'] += amount
 
     if choice == "3":
         amount = int(input("how many ice cubes do you want?\n"))
