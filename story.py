84f7d28834e3a9f883563c9b2ad293e2500cf870
Tuttle78 <zt539170@georgetownisd.org>
2026-03-09 12:49:39 -0500

    deleted story

--- story.py
+++
@@ -1,8 +0,0 @@
-def easyStart():
-    input("The new Call of Dut- I mean Signal of Job™ came out and all your friends got it. (enter to continue)")
-    input("Unfortunately for you, your parents told you they wouldn't buy it.")
-    input("So you decide to start saving up, and what better way to raise money than a lemonade stand!")
-    input("And right now's your best chance to get it, it's %50 off for the next 6 weeks, it's only $300!")
-    input("Luckily, your parents like this idea and decided to give some funds")
-def easyEnd():
-    print("Your finall")
